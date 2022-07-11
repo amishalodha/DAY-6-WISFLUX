0e19652c04b33340faa0572c470e5b9023ee8976
amishalodha <84517519+amishalodha@users.noreply.github.com>
2022-07-11 22:06:47 +0530

    Add files via upload

---
+++ sort an array.js
@@ -0,0 +1,6 @@
+const arr = new Array(1,54,78,3,24);
+
+  arr.sort();
+  console.log( arr);
+  arr.reverse();
+  console.log(arr);
---
+++ swap.js
@@ -0,0 +1,4 @@
+var x=21, y=45;
+[x,y]=[y,x];
+console.log(x);
+console.log(y);
